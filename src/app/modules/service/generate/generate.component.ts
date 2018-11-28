7868fb8eda7f9704377ebba454c1ceeeb9def368
大胃王 <253498229@qq.com>
2018-11-28 16:37:10 +0800

    生成服务

--- src/app/modules/service/generate/generate.component.ts
+++ src/app/modules/service/generate/generate.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-generate',
   templateUrl: './generate.component.html',
-  styleUrls: ['./generate.component.scss']
+  styleUrls: ['./generate.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GenerateComponent implements OnInit {
+  inputValue: string;
+  tags = [];
+  options = [];
+  model = {
+    group: 'com.example',
+    artifact: 'demo'
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onInput(value: string): void {
+    this.options = value ? [
+      value,
+      value + value,
+      value + value + value
+    ] : [];
+  }
+
+  remove(idx) {
+    this.tags.splice(idx, 1);
+  }
+
+  choose(option: any) {
+    this.tags.push(option);
+    this.inputValue = '';
+  }
 }
